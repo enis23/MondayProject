b85c8a316193043745d2b0a38c8008b85c7e6d9b
Apps at Techno Study <apps@techno.study>
2020-01-27 17:56:46 -0500

    Monday project

---
+++ readme.md
@@ -0,0 +1,21 @@
+###### Part 1
+
+go to https://mockaroo.com/ and generate id and gender excel file, download and place in resources folder
+
+go to https://mockaroo.com/ and generate id and gender sql table, create a database and put this table in that database
+
+create a test where you compare genders from mysql and excel by id
+
+expect around 50% of test cases to fail, it's ok
+###### Part 2
+
+go to https://mockaroo.com/ and generate id, name, gender, fee, and number field called multiplier sql table called students,
+ create a database and put this table in that database
+ 
+the multiplier field must be from 0.5 to 2.0 with 2 decimal places
+
+create a test case where update the student's fee using multiplier and write it to database, after updating to read from database again and assert that it's correctly updated
+ 
+ expect around 20-30% of test cases to fail
+ 
+
---
+++ src/test/java/TestCase.java
@@ -0,0 +1,29 @@
+
+
+public class TestCase {
+
+    public Object[][] genderData()  {
+        // select everything from gender table and generate Object[][] with id and gender columns
+        return null;
+    }
+
+    public void genderTest() {
+        // read from gender.xlsx, get gender value by id provided and assert that gender in excel is the same as gender in mysql
+    }
+
+    public Object[][] studentsData()  {
+        // select id and multiplier from students table and generate Object[][]
+        return null;
+    }
+
+    public void multiplierTest() {
+        // select student's fee from students table by id and save in variable for assertion
+
+        // update student's fee by id, multiply fee by multiplier
+
+        // select student's fee from students table by id and assert that it's updated according to multiplier within 0.004 delta
+
+    }
+
+
+}
